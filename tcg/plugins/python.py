bc4af29b0d3171c07959163b3a1b52b16485eca2
antoine moynault <antmo654@gmail.com>
2017-09-22 11:22:12 +0200

    python plugins: tcg_plugin_python module

--- tcg/plugins/python.py
+++ tcg/plugins/python.py
@@ -25,15 +25,19 @@
 # SOFTWARE.
 #
 
-import os, sys
-
-import qemu
+import os, sys, collections
 
 try:
-    import capstone
+    import qemu, capstone
 except ImportError as e:
     print >>sys.stderr, 'error: ' + e.message
     sys.exit(1)
+
+try:
+    from repoze.lru import lru_cache
+except:
+    print >>sys.stderr, 'warning: missing module repoze.lru!'
+    def lru_cache(maxsize=None): return lambda f: f
 
 try:
     assert 0x02070000 <= sys.hexversion < 0x03000000
@@ -44,25 +48,84 @@ except:
 
 # ############################################################################
 
+plugins = []
+
+capstone_md = None
+
+block_sym = {}  # { address : symbol }
+
+Symbol = collections.namedtuple('Symbol', ['name', 'address', 'size'])
+
+
+# ############################################################################
+
+@lru_cache(maxsize=None)
+def get_disas_code(address, size):
+    disas_code = capstone_md.disasm(qemu.getcode(address, size), address)
+    return list(disas_code)
+
+def get_symbol(address):
+    return block_sym.get(address, None)
+
+def plugins_callback(fct_name, *args):
+    for plugin in plugins:
+        try:
+            fct = getattr(plugin, fct_name)
+            fct(*args)
+        except AttributeError: pass
+        except: raise
+
+def import_plugin(plugin_filename):
+    # take basename and remove extension
+    plugin_filename = os.path.realpath(plugin_filename)
+    plugin_id = os.path.splitext(os.path.basename(plugin_filename))[0]
+    # try already installed python plugins
+    try:
+        module = __import__(plugin_id, globals(), locals())
+        return module
+    except ImportError: pass
+    except: raise
+    # try plugin relative to current working dir
+    if os.path.isfile(plugin_filename):
+        sys.path.append(os.path.dirname(plugin_filename))
+        module = __import__(plugin_id, globals(), locals())
+        return module
+    print >>sys.stderr, 'warning: plugin not found', plugin_id
+    return None
+
+
+# ############################################################################
 
 def on_progr_start(uname=None):
-    global md
-    assert uname == 'x86_64', 'unsupported target!'
-    md = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_64)
+    # import python plugins
+    plugin_ids = os.getenv('PYTHON_PLUGIN', '').split(',')
+    for plugin in plugin_ids:
+        plugin_module = import_plugin(plugin)
+        plugin_module and plugins.append(plugin_module)
 
+    # initialize capstone module
+    global capstone_md
+    assert uname == 'x86_64', 'unsupported target!'
+    capstone_md = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_64)
+
+    # initialize python plugins
+    plugins_callback('on_progr_start')
 
 def on_progr_end():
-    print 'end'
+    # finalize python plugins
+    plugins_callback('on_progr_end')
 
+def on_block_trans(address, size, sym_name=None, sym_address=None, sym_size=None):
+    # update block_to_symbol map
+    sym_name and block_sym.update([(address, Symbol(sym_name, sym_address, sym_size))])
 
-def on_block_trans(pc, size, sym_id=None, sym_pc=None, sym_size=None):
-    print >>sys.stderr, 'TRNS', hex(pc), size, sym_id, sym_pc, sym_size
+def on_block_exec(address, size):
+    # on_block_exec
+    plugins_callback('on_block_exec', address, size, block_sym.get(address, None))
 
-
-def on_block_exec(pc, size):
-    print >>sys.stderr, 'EXEC', hex(pc), size
-    for (address, size, mnemonic, op_str) in md.disasm_lite(qemu.getcode(pc, size), pc):
-       print '0x%x: %s %s' % (address, mnemonic, op_str)
+    # on_instr_exec
+    for instr in get_disas_code(address, size):
+        plugins_callback('on_instr_exec', instr)
 
 
 # ############################################################################
